780583c429a61b9ced84b1772bd2a2af3bd33d38
Giovanni Marigi <gio.marigi@gmail.com>
2026-04-28 23:11:46 +0200

    Add license and header comments to bike_setup.py
    Added licensing information and comments to bike_setup.py

--- src/bikescout/tools/bike_setup.py
+++ src/bikescout/tools/bike_setup.py
@@ -1,3 +1,19 @@
+# BikeScout - Tactical Intelligence for Cyclists
+# Copyright (C) 2026 hifly81 (https://github.com/hifly81/bikescout)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 def analyze_compatibility(bike_type: str, tire_mm: int, extras: dict, surface_map: dict):
     """
     Physics-based compatibility check using tire width (mm) and bike geometry.
